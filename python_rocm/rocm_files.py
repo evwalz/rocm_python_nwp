7dda361571733cbcec49a0d5897e9e70f32cec71
evwalz <56073009+evwalz@users.noreply.github.com>
2020-04-16 08:03:33 +0200

    Update rocm_files.py
    update

--- python_rocm/rocm_files.py
+++ python_rocm/rocm_files.py
@@ -9,57 +9,84 @@ import numpy as np
 from scipy.stats import rankdata
 from scipy import interp
 import matplotlib.pyplot as plt
+import imageio
 
-# own functions:
-from uroc import uroc, Trapezoidal
+# own functions
+from uroc_approx import uroc_app, Trapezoidal
 
-def rocm(response, predictor, path):
+def rocm(response, predictor, pathGIF, pathPNG):
     """
-    Creates a sequence of ROC curves and saves them to path.
+    Creates a ROC movie and saves it to pathGIF. Creates a UROC curve and saves it to pathPNG
 
     Visualizes the performances of predictors for real valued forecasting problems. 
 
     Parameters
     ----------
     response : 1D array_like, 1-D array containing obervation. Need to have the same length in the ``axis`` dimension as b.
-	predictor : 1D array_like, 1-D array containing forecast for observation a.
-    path: String, Example: /home/
+    predictor : 1D array_like, 1-D array containing forecast for observation a.
+    pathGIF: String, Example: /home/rocm.gif
+    pathPNG: String, Example: /home/uroc.png
        
     Returns
     -------
-    Several png-files
+    gif-file
+    png-file
     """ 
-    n = response.shape[0]
-    # order response and predictor increasing by response
+    response = np.array(response)
+    predictor = np.array(predictor)
     response_sorted_indices = np.argsort(response)
     response_sorted = response[response_sorted_indices]
     predictor_sorted = predictor[response_sorted_indices]
     response_unique, response_unique_index = np.unique(response_sorted, return_index=True)
     N = response_unique.shape[0]-1
+    n = response.shape[0]
+    if N == 0:
+        raise ValueError('response must have more than one level')
     a = 400
-    b = 1
+    b = 100
     if a > N:
-        a = N
-    s = np.floor((N - 1) / (a - 1))
-    hh = (2 + (a - 1) * s)
-    indx_setCa = np.arange(1, hh , s)
-    class_length = np.subtract(np.append(response_unique_index[2:],n),response_unique_index[1:])
-    indx_setCb = np.where(class_length>n/b)
-    indxsetC = np.sort(np.unique(np.concatenate((indx_setCa, indx_setCb), axis = None)))
+        indx_setC = np.arange(1, (N+1), 1)
+        indxuroc = indx_setC
+        if N > 100:
+        #duration = 1/10
+            fps = 10
+        else:
+        #duration = 1/(np.floor(N/10))
+            fps = np.floor(N/10)+1
+        
+    else:
+        if N <= 1000:
+            split = 1
+        elif N <= 10000:
+            split = 10 
+        elif N <= 100000:
+            split = 500
+        elif N <= 1000000:
+            split = 5000
+    #duration = 1/20
+        fps = 20
+        s = np.floor((N - 1) / (a - 1))
+        hh = (1 + (a - 1) * s)
+        indx_setCa = np.arange(1, hh , s)
+        indx_setCauroc = np.arange(1, N, split)
+        class_length = np.subtract(np.append(response_unique_index[2:],n),response_unique_index[1:])
+        indx_setCb = np.where(class_length>n/b)
+        indx_setC = np.sort(np.unique(np.concatenate((indx_setCa, indx_setCb), axis = None)))
+        indxuroc = np.sort(np.unique(np.concatenate((indx_setCauroc, indx_setCb), axis = None)))
 
-    # compute weights
     ncontrol = (response_unique_index)[1:]
-    ncontrol_split = response_unique_index[indxsetC.astype(int)]
-    thresholds_split = response_unique[indxsetC.astype(int)]
-    ncases_split = n - ncontrol_split
-    weights_split = np.multiply(ncases_split , ncontrol_split)
+    ncontrol_uroc = response_unique_index[indxuroc.astype(int)]
+    ncontrol_split = response_unique_index[indx_setC.astype(int)]
+    thresholds_split = response_unique[indx_setC.astype(int)]
+    weights_split = np.multiply(ncontrol_split, n-ncontrol_split)
     weights_all = np.multiply(ncontrol, n-ncontrol)
     weights_scale = weights_split / np.max(weights_all)
 
-    classes_predictor = rankdata(predictor_sorted, method='dense')
-    Split_classes_predictor = np.split(classes_predictor[:response_unique_index[-1]],ncontrol_split[:-1])
 
-    # compute first roc curve
+    classes_predictor = rankdata(predictor_sorted, method='dense')
+    Split_classes_uroc = np.split(classes_predictor[:response_unique_index[-1]],ncontrol_uroc[:-1])
+    Split_classes_predictor = np.split(classes_predictor[:response_unique_index[-1]],ncontrol_split[:-1])
+    # First roc curve
     order_predictor = predictor.argsort()[::-1]
     response_binary = np.where(np.array(response[order_predictor]) > response_unique[0], 1, 0)
     predictor_sorted = predictor[order_predictor][::-1]
@@ -69,16 +96,26 @@ def rocm(response, predictor, path):
     fpr = np.insert(np.cumsum(response_binary == 0)[dups], 0, 0)
     tpr_weight = tpr[::-1]
     fpr_weight = fpr[::-1]
+
+
     InterPoint = np.arange(0, 1001, 1) * 0.001
-    cases = n- ncontrol_split[0]
-    hitrate = np.array(interp(InterPoint, fpr/ncontrol_split[0], tpr/cases))
+
+    cases = n- ncontrol_uroc[0]
+    hitrate = np.array(interp(InterPoint, fpr/ncontrol_uroc[0], tpr/cases))
+
     auc = np.round(Trapezoidal(InterPoint, hitrate),2)
     hitrate = np.append(0, hitrate)
     InterPoint_zero = np.append(0, InterPoint)
     sum_tpr_fpr = np.sum([fpr_weight, tpr_weight], axis=0)
+    lenindx = indxuroc.shape[0]
+    tpru = list(tpr) 
+    fpru = list(fpr)
+    far_uroc, hit_uroc = uroc_app(ncontrol_uroc, tpru, fpru, n, Split_classes_uroc, lenindx)
+
     w = np.round(weights_scale[0],2)
     z = np.round(thresholds_split[0],2)
-    fig = plt.figure()
+    images = []
+    fig = plt.figure(figsize=(6,6))
     plt.plot(InterPoint_zero,hitrate)
     plt.plot([0,1],[0,1], '--', color='grey')
     plt.ylabel('Sensitivity')
@@ -86,11 +123,14 @@ def rocm(response, predictor, path):
     plt.text(0.7,0.2,'AUC: {:3.2f}'.format(auc))
     plt.text(0.17,0.95,'z: %s'%(z))
     plt.text(0.0,0.95,'w: {:3.2f}'.format(w))
-    name = 'animate_0.png'
-    fig.savefig(path + name)
+    fig.canvas.draw()       # draw the canvas, cache the renderer
+    image = np.frombuffer(fig.canvas.tostring_rgb(), dtype='uint8')
+    image = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    images.append(image)
     plt.close()
 
-    for i in range(1,(indxsetC.shape[0])):
+
+    for i in range(1,(indx_setC.shape[0])):
     
         sorted_split_element = np.sort(np.append(Split_classes_predictor[i],0))
         diff_split_element = np.subtract(sorted_split_element[1:],sorted_split_element[:-1])
@@ -106,7 +146,7 @@ def rocm(response, predictor, path):
         hitrate = np.append(0, hitrate)
         w = np.round(weights_scale[i],2)
         z = np.round(thresholds_split[i],2)
-        fig = plt.figure()
+        fig = plt.figure(figsize=(6,6))
         plt.plot(InterPoint_zero,hitrate)
         plt.plot([0,1],[0,1], '--', color='grey')
         plt.ylabel('Sensitivity')
@@ -114,13 +154,27 @@ def rocm(response, predictor, path):
         plt.text(0.7,0.2,'AUC: {:3.2f}'.format(auc))
         plt.text(0.17,0.95,'z: %s'%(z))
         plt.text(0.0,0.95,'w: {:3.2f}'.format(w))
-        name = 'animate_' + str(i) + '.png'
-        fig.savefig(path + name)
+        fig.canvas.draw()       # draw the canvas, cache the renderer
+        image = np.frombuffer(fig.canvas.tostring_rgb(), dtype='uint8')
+        image  = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+        images.append(image)
         plt.close()
 
-    fig = plt.figure()    
-    uroc(response, predictor) 
-    name = 'animate_' + str(indxsetC.shape[0]) + '.png'
+
+    cpa_approx = np.round(Trapezoidal(far_uroc, hit_uroc),2)
+    fig = plt.figure(figsize=(6,6))
+    plt.plot(far_uroc,hit_uroc)
+    plt.plot([0,1],[0,1], '--', color='grey')
+    plt.ylabel('Sensitivity')
+    plt.xlabel('1 - Specificity')
+    plt.text(0.7,0.2,'CPA: {:3.2f}'.format(cpa_approx))
     plt.title('UROC curve')
-    fig.savefig(path+name)
+    plt.savefig(pathPNG)
+    fig.canvas.draw()       # draw the canvas, cache the renderer
+    image = np.frombuffer(fig.canvas.tostring_rgb(), dtype='uint8')
+    image  = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    images.append(image)
     plt.close()
+    
+
+    imageio.mimsave(pathGIF, [images[i] for i in (range((len(images))))], fps=fps, loop=1)  
